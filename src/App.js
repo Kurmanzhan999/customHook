e305cc1af1e5df2b8d9a8ef75daec2977c9c7da4
kurmanzhan Muhtarova <kurmanzhanmuhtarova@gmail.com>
2021-12-07 22:54:31 -1000

    new branch

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from "react";
-import BasicForm from "./components/BasicForm";
+
 
 function App() {
   return (
     <div >
-      {/* <SimpleInput /> */}
-      <BasicForm/>
+    
     </div>
   );
 }
